47f7b68aea0b29ae588b4cc05b057b6b3a1ccedc
fadhilakamila <fadhilakamilaa@gmail.com>
2023-12-08 12:25:05 +0700

    inorder

--- src/treeTraversal/BinaryTree.java
+++ src/treeTraversal/BinaryTree.java
@@ -1,6 +1,5 @@
 package treeTraversal;
 
-
 public class BinaryTree {
     Node root;
 
@@ -9,17 +8,28 @@ public class BinaryTree {
         root = addRecursive(root, node);
     }
 
-    // posisi current dibandingkan dengan yang mau dimasukkan. Node di parameter tsb adalah node yang mau dimasukin
+    // posisi current (root setiap level) dibandingkan dengan yang mau dimasukkan. Node di parameter tsb adalah node yang mau dimasukin
     public Node addRecursive(Node current, Node node) { // method untuk membuat tree baru
         if (current == null) {
-            return node; // node di sini dimasukkan ke dalam root di line 9
+            return node; // node di sini dimasukkan ke dalam root di line 8
         } else if (current.value > node.value) {
             current.left = addRecursive(current.left, node);
         } else if (current.value < node.value) {
-            current.right =addRecursive(current.right, node);
+            current.right = addRecursive(current.right, node);
         } else {
             return current;
         }
         return current;
+    }
+
+    public void printInorder(Node node) {
+        if (node == null)
+            return;
+
+        printInorder(node.left);
+
+        System.out.print(node.value + " ");
+
+        printInorder(node.right);
     }
 }
--- src/treeTraversal/Main.java
+++ src/treeTraversal/Main.java
@@ -3,27 +3,33 @@ package treeTraversal;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("nyoba1");
-
         BinaryTree binaryTree = new BinaryTree();
 
         Node empat = new Node(4);
         Node tujuh = new Node(7);
         Node lima = new Node(5);
+        Node satu = new Node(1);
+        Node sepuluh = new Node(10);
 
         binaryTree.add(empat);
         binaryTree.add(tujuh);
         binaryTree.add(lima);
+        binaryTree.add(satu);
+        binaryTree.add(sepuluh);
 
+        // nge-cek siapa root-nya
         System.out.println(binaryTree.root.value);
 
         // nge-cek siapa yang ada di kirinya root
-//        System.out.println(binaryTree.root.left.value);
+        System.out.println(binaryTree.root.left.value);
 
         // nge-cek siapa yang ada di kanannya root
         System.out.println(binaryTree.root.right.value);
 
         System.out.println(binaryTree.root.right.left.value);
+
+        System.out.println("Inorder traversal of binary tree is");
+        binaryTree.printInorder(binaryTree.root);
 
 
     }
