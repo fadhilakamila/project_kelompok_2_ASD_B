37d40535daef419ff6682cea107a2ef051096a32
fadhilakamila <fadhilakamilaa@gmail.com>
2023-12-15 13:35:03 +0700

    consoleOO State Class

--- src/ticTacToe/consoleOO/State.java
+++ src/ticTacToe/consoleOO/State.java
@@ -1,20 +1,13 @@
-//package ticTacToe.consoleOO;
-//
-//public class State {
-//    // Define named constants to represent the various states of the game
-//    public static final int PLAYING    = 0;
-//    public static final int DRAW       = 1;
-//    public static final int CROSS_WON  = 2;
-//    public static final int NOUGHT_WON = 3;
-//
-//    // The current state of the game
-//    public static int currentState = PLAYING;  // Assigned to a named constant, which is easier to read
-//    // and understand, instead of an int number 0
-//
-//    currentState = 99;   // A logical error but can compile
-//
-//    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-//
-//    State currentState;            // declare variable currentState as an instance of enum Gamee
-//    currentState = State.PLAYING;  // assign a value (an enum item) to the variable currentState
-//}
+package ticTacToe.consoleOO;
+
+public class State {
+    // Define named constants to represent the various states of the game
+    public static final int PLAYING    = 0;
+    public static final int DRAW       = 1;
+    public static final int CROSS_WON  = 2;
+    public static final int NOUGHT_WON = 3;
+
+    // The current state of the game
+    public static int currentState = PLAYING;  // Assigned to a named constant, which is easier to read
+    // and understand, instead of an int number 0
+}
